0b2722f48083584db082a2c844246a453cfc1e23
b3mb4m <b3mb4m@tuta.io>
2016-02-15 11:15:19 +0200

    Start coding ..

---
+++ masm/compile.py
@@ -0,0 +1,25 @@
+#Masm complie script
+#https://github.com/b3mb4m/Shellcode/blob/master/Auxiliary/masm/complie.py
+
+
+import os
+import sys
+
+if len(sys.argv) < 2:
+	sys.exit("python complie.py [FILENAME]")
+
+try:
+	_= "ml.exe /c /coff %s" % sys.argv[1]
+	__= "link.exe /SUBSYSTEM:WINDOWS %s" % (sys.argv[1].split(".")[0]+".obj")
+
+	os.system(_)
+	os.system(__)
+
+	if os.path.isfile(sys.argv[1].split(".")[0]+".exe"):
+		print ("\nYour exe file ready !\n")
+		print (sys.argv[1])
+		print (sys.argv[1].split(".")[0]+".exe")
+	os.remove(sys.argv[1].split(".")[0]+".obj")
+
+except Exception as error:
+	sys.exit(error)	
